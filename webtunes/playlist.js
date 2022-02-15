b0c496d81e17702f9051303338a107a6e13ae59d
Jackson T. Curtis <puajr47@gmail.com>
2022-02-15 00:20:52 -0500

    fixed syntax error with function

--- webtunes/playlist.js
+++ webtunes/playlist.js
@@ -1,10 +1,9 @@
 window.onload = init; function init() {
 var button = document.getElementById("addButton");
-button.onclick = handleButtonClick;
-}
 function handleButtonClick() {
     var textInput = document.getElementById("songTextInput"); var songName = textInput.value;
     if (songName == "") {
     alert("Please enter a song"); } else {
     alert("adding " + songName); }
-    }
+    }button.onclick = handleButtonClick;
+}
