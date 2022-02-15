ba727e5870f67116bfc8d90473e70588a387e841
Jackson T. Curtis <puajr47@gmail.com>
2022-02-15 00:18:10 -0500

    updated button click  function

--- webtunes/playlist.js
+++ webtunes/playlist.js
@@ -2,7 +2,9 @@ window.onload = init; function init() {
 var button = document.getElementById("addButton");
 button.onclick = handleButtonClick;
 }
-function handleButtonClick()    {
-    alert("Button was clicked!");
-} 
-
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput"); var songName = textInput.value;
+    if (songName == "") {
+    alert("Please enter a song"); } else {
+    alert("adding " + songName); }
+    }
